29568a6ced21fe806b5b750632825c34d42ba1a2
Furkan Çetintaş <66469657+furkancetintas@users.noreply.github.com>
2022-09-06 14:48:05 +0300

    main.js added (and elements selected)

---
+++ main.js
@@ -0,0 +1,8 @@
+let body = document.querySelector('body');
+let generateColorBtn = document.querySelector('#generate');
+let hexTextsDOM = document.querySelectorAll('.hex-text');
+let copiedSpanDOM = document.getElementById('copied');
+
+let cardColors = document.querySelectorAll('.card-color');
+
+let colorDetails = document.querySelector('.color-details');
