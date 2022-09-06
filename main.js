a633350f1719688d0345d345851db2f90913e03e
Furkan Çetintaş <66469657+furkancetintas@users.noreply.github.com>
2022-09-06 16:30:45 +0300

    main.js updated! done..

--- main.js
+++ main.js
@@ -6,3 +6,38 @@ let copiedSpanDOM = document.getElementById('copied');
 let cardColors = document.querySelectorAll('.card-color');
 
 let colorDetails = document.querySelector('.color-details');
+
+const randomHexColor = () => {
+  cardColors.forEach((cardColor) => {
+    let hexColor = (Math.random() * 0xffffff * 1000000).toString(16);
+    let result = '#' + hexColor.slice(0, 6);
+    hexTextsDOM.forEach((hexText) => {
+      hexText.innerHTML = result;
+    });
+    cardColor.style.background = result;
+    cardColor.addEventListener('click', () => {
+      colorDetails.style.display = 'flex';
+      colorDetails.style.background = result;
+      let inputCreateDOM = document.createElement('input');
+      inputCreateDOM.value = result;
+      document.body.appendChild(inputCreateDOM);
+      inputCreateDOM.select();
+      document.execCommand('copy');
+      inputCreateDOM.style.display = 'none';
+      document.body.style.overflow = 'hidden';
+      let copiedTextTimeout = setTimeout(() => {
+        colorDetails.style.display = 'none';
+        document.body.style.overflow = 'visible';
+        clearInterval(copiedTextTimeout);
+      }, 1500);
+    });
+
+    cardColor.innerHTML = `
+      <h3 class="hex-text">${result}</h3>
+    `;
+  });
+};
+
+generateColorBtn.addEventListener('click', randomHexColor);
+
+randomHexColor();
